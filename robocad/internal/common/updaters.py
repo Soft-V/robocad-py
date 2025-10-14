7514e40968ba7c3fa1c1107b93ad879d83c31731
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2025-10-14 15:02:43 +0300

    Repka updater works

--- robocad/internal/common/updaters.py
+++ robocad/internal/common/updaters.py
@@ -1,4 +1,5 @@
 import time
+import io
 from abc import ABC
 
 from .robot import Robot
@@ -13,22 +14,30 @@ class RpiUpdater(Updater):
         super().__init__(robot)
     
     def updater(self):
-        from gpiozero import CPUTemperature # type: ignore
-        import psutil # type: ignore
-        cpu_temp: CPUTemperature = CPUTemperature()
-        while not self.stop_robot_info_thread:
-            self.robot.robot_info.temperature = cpu_temp.temperature
-            self.robot.robot_info.memory_load = psutil.virtual_memory().percent
-            self.robot.robot_info.cpu_load = psutil.cpu_percent(interval=0.5)
-            time.sleep(0.5)
+        try:
+            from gpiozero import CPUTemperature # type: ignore
+            import psutil # type: ignore
+            cpu_temp: CPUTemperature = CPUTemperature()
+            while not self.stop_robot_info_thread:
+                self.robot.robot_info.temperature = cpu_temp.temperature
+                self.robot.robot_info.memory_load = psutil.virtual_memory().percent
+                self.robot.robot_info.cpu_load = psutil.cpu_percent(interval=0.5)
+                time.sleep(0.5)
+        except Exception as e:
+            self.robot.write_log(str(e))
 
-class ElveesUpdater(Updater):
+class RepkaUpdater(Updater):
     def __init__(self, robot: Robot):
         super().__init__(robot)
     
     def updater(self):
-        while not self.stop_robot_info_thread:
-            self.robot.robot_info.temperature = 0
-            self.robot.robot_info.memory_load = 0
-            self.robot.robot_info.cpu_load = 0
-            time.sleep(0.5)
+        try:
+            import psutil # type: ignore
+            while not self.stop_robot_info_thread:
+                with io.open('/sys/class/thermal/thermal_zone0/temp', 'r') as f:
+                    self.robot.robot_info.temperature = float(f.read().strip()) / 1000
+                self.robot.robot_info.memory_load = psutil.virtual_memory().percent
+                self.robot.robot_info.cpu_load = psutil.cpu_percent(interval=0.5)
+                time.sleep(0.5)
+        except Exception as e:
+            self.robot.write_log(str(e))
