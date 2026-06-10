3a1f17089cb0e0d12fecd5205c9edd08e48f3d95
Airat Abdrakov <52558686+CrackAndDie@users.noreply.github.com>
2026-06-10 10:55:20 +0300

    Simulator part for algaritm added

--- robocad/internal/algaritm_internal.py
+++ robocad/internal/algaritm_internal.py
@@ -78,10 +78,9 @@ class AlgaritmInternal:
 
         self.__connection: ConnectionBase = None
         if not self.__robot.on_real_robot:
-            pass
-            # self.__connection = ConnectionSim(self.__robot)
-            # self.__robocad_conn = RobocadConnection()
-            # self.__robocad_conn.start(self.__connection, self.__robot, self)
+            self.__connection = ConnectionSim(self.__robot)
+            self.__robocad_conn = RobocadConnection()
+            self.__robocad_conn.start(self.__connection, self.__robot, self)
         else:
             updater = RepkaUpdater(self.__robot)
             self.__connection = ConnectionReal(self.__robot, updater, conf)
@@ -93,9 +92,8 @@ class AlgaritmInternal:
     def stop(self):
         self.__connection.stop()
         if not self.__robot.on_real_robot:
-            pass
-            # if self.__robocad_conn is not None:
-                # self.__robocad_conn.stop()
+            if self.__robocad_conn is not None:
+                self.__robocad_conn.stop()
         else:
             if self.__titan is not None:
                 self.__titan.stop()
@@ -120,6 +118,116 @@ class AlgaritmInternal:
             self.step_motor_2_steps = steps
             self.step_motor_2_steps_per_s = steps_per_second
             self.step_motor_2_direction = direction
+
+
+class RobocadConnection:
+    def __init__(self):
+        self.__update_thread = None
+        self.__stop_update_thread = False
+
+    def start(self, connection: ConnectionSim, robot: Robot, robot_internal: AlgaritmInternal):
+        self.__connection: ConnectionSim = connection
+        self.__robot: Robot = robot
+        self.__robot_internal: AlgaritmInternal = robot_internal
+
+        self.__robot.power = 12  # todo: control from ConnectionSim from robocad
+
+        self.__stop_update_thread = False
+        self.__update_thread = Thread(target=self.__update)
+        self.__update_thread.daemon = True
+        self.__update_thread.start()
+
+    def stop(self):
+        self.__stop_update_thread = True
+        if self.__update_thread is not None:
+            self.__update_thread.join()
+    
+    def __set_data(self, values: tuple) -> None:
+        self.__connection.set_data(RobocadConnection.join_algaritm_channel(values))
+
+    def __get_data(self) -> tuple:
+        return RobocadConnection.parse_algaritm_channel(self.__connection.get_data())
+    
+    def __update(self):
+        while not self.__stop_update_thread:
+            # set data
+            values = [self.__robot_internal.speed_motor_0,
+                      self.__robot_internal.speed_motor_1,
+                      self.__robot_internal.speed_motor_2,
+                      self.__robot_internal.speed_motor_3]
+            values.extend(self.__robot_internal.servo_angles)
+            values.append(self.__robot_internal.additional_servo_1)
+            values.append(self.__robot_internal.additional_servo_2)
+            values.append(self.__robot_internal.step_motor_1_steps)
+            values.append(self.__robot_internal.step_motor_2_steps)
+            values.append(self.__robot_internal.step_motor_1_steps_per_s)
+            values.append(self.__robot_internal.step_motor_2_steps_per_s)
+            values.append(1 if self.__robot_internal.step_motor_1_direction else 0)
+            values.append(1 if self.__robot_internal.step_motor_2_direction else 0)
+            values.append(1 if self.__robot_internal.use_pid else 0)
+            values.append(self.__robot_internal.p_pid)
+            values.append(self.__robot_internal.i_pid)
+            values.append(self.__robot_internal.d_pid)
+            values.append(1 if self.__robot_internal.outputs[0] else 0)
+            values.append(1 if self.__robot_internal.outputs[1] else 0)
+            values.append(1 if self.__robot_internal.outputs[2] else 0)
+            values.append(1 if self.__robot_internal.outputs[3] else 0)
+            self.__set_data(tuple(values))
+
+            # get data
+            values = self.__get_data()
+            if len(values) > 0:
+                self.__robot_internal.enc_motor_0 = values[0]
+                self.__robot_internal.enc_motor_1 = values[1]
+                self.__robot_internal.enc_motor_2 = values[2]
+                self.__robot_internal.enc_motor_3 = values[3]
+                self.__robot_internal.ultrasound_1 = values[4]
+                self.__robot_internal.ultrasound_2 = values[5]
+                self.__robot_internal.ultrasound_3 = values[6]
+                self.__robot_internal.ultrasound_4 = values[7]
+                self.__robot_internal.analog_1 = values[8]
+                self.__robot_internal.analog_2 = values[9]
+                self.__robot_internal.analog_3 = values[10]
+                self.__robot_internal.analog_4 = values[11]
+                self.__robot_internal.analog_5 = values[12]
+                self.__robot_internal.analog_6 = values[13]
+                self.__robot_internal.analog_7 = values[14]
+                self.__robot_internal.analog_8 = values[15]
+                self.__robot_internal.yaw = values[16]
+                self.__robot_internal.pitch = values[17]
+                self.__robot_internal.roll = values[18]
+
+                self.__robot_internal.limit_h_0 = values[19] == 1
+                self.__robot_internal.limit_l_0 = values[20] == 1
+                self.__robot_internal.limit_h_1 = values[21] == 1
+                self.__robot_internal.limit_l_1 = values[22] == 1
+                self.__robot_internal.limit_h_2 = values[23] == 1
+                self.__robot_internal.limit_l_2 = values[24] == 1
+                self.__robot_internal.limit_h_3 = values[25] == 1
+                self.__robot_internal.limit_l_3 = values[26] == 1
+
+                self.__robot_internal.inputs[0] = values[27] == 1
+                self.__robot_internal.inputs[1] = values[28] == 1
+                self.__robot_internal.inputs[2] = values[29] == 1
+                self.__robot_internal.inputs[3] = values[30] == 1
+
+                self.__robot_internal.is_step_1_busy = values[31] == 1
+                self.__robot_internal.is_step_2_busy = values[32] == 1
+            
+            # задержка для слабых компов
+            time.sleep(0.004)
+    
+    @staticmethod
+    def join_algaritm_channel(lst: tuple) -> bytes:
+        if len(lst) < 28:
+            return b''
+        return struct.pack('28f', *lst)
+    
+    @staticmethod
+    def parse_algaritm_channel(data: bytes) -> tuple:
+        if len(data) < 74:
+            return tuple()
+        return struct.unpack('<4i4f8H3f14B', data)
 
     
 class TitanCOM:
